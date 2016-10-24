7b5b0c06d47b67ba20abcd1069078e5e5681c5ee
bbortt <timon.borter@gmx.ch>
2016-10-24 13:41:42 +0200

    map works. setting point works BUT not on map. exporting does not take point. code style should be reworked.

--- app/src/main/java/ch/jmelab/treasure_map/MainActivity.java
+++ app/src/main/java/ch/jmelab/treasure_map/MainActivity.java
@@ -1,11 +1,10 @@
 package ch.jmelab.treasure_map;
 
-import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Criteria;
@@ -16,11 +15,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.MapTileProviderArray;
@@ -37,11 +39,14 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.TilesOverlay;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity implements LocationListener {
     // Static variables
     private static final int REQUEST_APP_SETTINGS = 168;
-    private static final int FILE_SELECT_CODE = 0;
+    private static final String PREFERENCES_NAME = "Treasure_Map_Preferences";
 
     // Map view
     MapView map;
@@ -56,54 +61,24 @@ public class MainActivity extends Activity implements LocationListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Load local map
-        // Possibility of file picker here
-        setUpMapView(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/hsr.mbtiles"));
-
         // Initalize android GPS
-        initalizeLocationTrackerAndCheckPermission();
+        if (initalizeLocationTrackerAndCheckPermission()) {
+            // Load local map
+            // Possibility of file picker here
+            File mapFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/hsr.mbtiles");  // For example getFileFromUser();
+            setUpMapView(mapFile);
 
-        // Add listener to buttons
-        addButtonListeners();
-    }
+            // Add listener to buttons
+            addButtonListeners();
 
-    private void setUpMapView(File databaseFile) {
-        // Check if map file exists
-        if (!databaseFile.exists()) {
-            Log.e(getClass().toString(), "Database file does not exist at: " + databaseFile.getAbsolutePath());
-            return;
+            // Finished build
+            Log.i(getClass().toString(), "Application started successfully.");
+        } else {
+            Log.e(getClass().toString(), "Permission not granted! Exiting..");
         }
-
-        map = (MapView) findViewById(R.id.mapview);
-        map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-
-        map.setMultiTouchControls(true);
-        map.setBuiltInZoomControls(true);
-
-        controller = map.getController();
-        controller.setZoom(18);
-
-        // Die TileSource beschreibt die Eigenschaften der Kacheln die wir anzeigen
-        XYTileSource treasureMapTileSource = new XYTileSource("mbtiles", ResourceProxy.string.offline_mode, 1, 20, 256, ".png", "http://127.0.0.1");
-
-        /* Das verwenden von mbtiles ist leider ein wenig aufwändig, wir müssen
-         * unsere XYTileSource in verschiedene Klassen 'verpacken' um sie dann
-         * als TilesOverlay über der Grundkarte anzuzeigen.
-         */
-        MapTileModuleProviderBase treasureMapModuleProvider = new MapTileFileArchiveProvider(new SimpleRegisterReceiver(this),
-                treasureMapTileSource, new IArchiveFile[]{MBTilesFileArchive.getDatabaseFileArchive(databaseFile)});
-
-        MapTileProviderBase treasureMapProvider = new MapTileProviderArray(treasureMapTileSource, null,
-                new MapTileModuleProviderBase[]{treasureMapModuleProvider});
-
-        TilesOverlay treasureMapTilesOverlay = new TilesOverlay(treasureMapProvider, getBaseContext());
-        treasureMapTilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
-
-        // Jetzt können wir den Overlay zu unserer Karte hinzufügen:
-        map.getOverlays().add(treasureMapTilesOverlay);
     }
 
-    private void initalizeLocationTrackerAndCheckPermission() {
+    private boolean initalizeLocationTrackerAndCheckPermission() {
         // Check permission given
         String filePermission = "android.permission.WRITE_EXTERNAL_STORAGE";
         String gpsPermission = "android.permission.ACCESS_FINE_LOCATION";
@@ -111,6 +86,8 @@ public class MainActivity extends Activity implements LocationListener {
 
         // Ask for permissions if not granted yet
         if (!granted) {
+            Log.e(getClass().toString(), "Missing permissions. Asking user to grant..");
+
             new AlertDialog.Builder(this)
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .setTitle("Application permissions")
@@ -129,32 +106,224 @@ public class MainActivity extends Activity implements LocationListener {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             // Exit applicaiton on no
-                            android.os.Process.killProcess(android.os.Process.myPid());
+                            Log.e(getClass().toString(), "User denied application permission.");
                         }
                     })
                     .show();
         }
 
+        granted = (checkCallingOrSelfPermission(gpsPermission) == PackageManager.PERMISSION_GRANTED && checkCallingOrSelfPermission(filePermission) == PackageManager.PERMISSION_GRANTED);
+
         if (granted) {
+            Log.d(getClass().toString(), "All permissions granted. Building location service..");
+
             // Update location listener
             if (locationManager == null) {
                 locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-                Criteria criteria = new Criteria();
-                provider = locationManager.getBestProvider(criteria, true);
             }
 
+            // Check if GPS enabled
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                Log.e(getClass().toString(), "GPS not enabled yet!");
+                onProviderDisabled(provider);
+            } else {
+                Log.d(getClass().toString(), "GPS already enabled.");
+            }
+
+            // Get best provider
+            Criteria criteria = new Criteria();
+            provider = locationManager.getBestProvider(criteria, true);
+
+            // Request location updates
             try {
                 locationManager.requestLocationUpdates(provider, 10, 10, this);
             } catch (SecurityException e) {
                 Log.e(getClass().toString(), e.getLocalizedMessage());
             }
+
+            return true;
         }
+
+        return false;
+    }
+
+    private void setUpMapView(File databaseFile) {
+        // Check if map file exists
+        if (!databaseFile.exists()) {
+            Log.e(getClass().toString(), "Database file does not exist at: " + databaseFile.getAbsolutePath());
+            return;
+        } else {
+            Log.d(getClass().toString(), "Opening database from: " + databaseFile.getAbsolutePath());
+        }
+
+        map = (MapView) findViewById(R.id.mapview);
+        map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+
+        map.setMultiTouchControls(true);
+        map.setBuiltInZoomControls(true);
+
+        controller = map.getController();
+        controller.setZoom(18);
+
+        // Die TileSource beschreibt die Eigenschaften der Kacheln die wir anzeigen
+        XYTileSource treasureMapTileSource = new XYTileSource("mbtiles", ResourceProxy.string.offline_mode, 1, 20, 256, ".png", new String[]{});
+
+        /* Das verwenden von mbtiles ist leider ein wenig aufwändig, wir müssen
+         * unsere XYTileSource in verschiedene Klassen 'verpacken' um sie dann
+         * als TilesOverlay über der Grundkarte anzuzeigen.
+         */
+        MapTileModuleProviderBase treasureMapModuleProvider = new MapTileFileArchiveProvider(new SimpleRegisterReceiver(this),
+                treasureMapTileSource, new IArchiveFile[]{MBTilesFileArchive.getDatabaseFileArchive(databaseFile)});
+
+        MapTileProviderBase treasureMapProvider = new MapTileProviderArray(treasureMapTileSource, null,
+                new MapTileModuleProviderBase[]{treasureMapModuleProvider});
+
+        TilesOverlay treasureMapTilesOverlay = new TilesOverlay(treasureMapProvider, getBaseContext());
+        treasureMapTilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
+
+        // Jetzt können wir den Overlay zu unserer Karte hinzufügen:
+        map.getOverlays().add(treasureMapTilesOverlay);
     }
 
     private void addButtonListeners() {
         // Mark point button
+        ImageButton markPointButton = (ImageButton) findViewById(R.id.imagebuttonpoint);
+        markPointButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // Check GPS connection
+                    if (locationManager.getLastKnownLocation(provider) == null) {
+                        Log.e(getClass().toString(), "Location unknown! Cannot set mark..");
+                        return;
+                    }
+
+                    // Acccess shared preferences
+                    SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+                    SharedPreferences.Editor preferencesEditor = sharedPreferences.edit();
+
+                    // Add point
+                    Log.i(getClass().toString(), "Adding location marrk at actual point: " + locationManager.getLastKnownLocation(provider).getLatitude() + "/" + locationManager.getLastKnownLocation(provider).getLongitude());
+                    long timeStamp = System.currentTimeMillis() / 1000;
+                    preferencesEditor.putLong("Latitude at " + timeStamp, Double.doubleToRawLongBits(locationManager.getLastKnownLocation(provider).getLatitude()));
+                    preferencesEditor.putLong("Altitude at " + timeStamp, Double.doubleToRawLongBits(locationManager.getLastKnownLocation(provider).getAltitude()));
+                    preferencesEditor.commit();
+                } catch (SecurityException e) {
+                    Log.e(getClass().toString(), e.getLocalizedMessage());
+                }
+            }
+        });
 
         // Export button
+        ImageButton exportButton = (ImageButton) findViewById(R.id.imagebuttonexport);
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                // User must confirm export
+                new AlertDialog.Builder(v.getContext())
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Export data")
+                        .setMessage("Are you sure you want to export your collected data now?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Open applicaiton settings
+                                Log.i(getClass().toString(), "Confirmed exporting marks to logbook..");
+
+                                // Acccess shared preferences
+                                SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+
+                                Intent intent = new Intent("ch.appquest.intent.LOG");
+
+                                if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+                                    Toast.makeText(v.getContext(), "Logbook App not installed", Toast.LENGTH_LONG).show();
+                                    return;
+                                }
+
+                                JSONObject jsonMessage = new JSONObject();
+                                try {
+                                    // First part of json message
+                                    jsonMessage.put("task", "Schatzkarte");
+                                    JSONArray jsonPoints = new JSONArray();
+
+                                    // Prerequests
+                                    Map<String, ?> sharedPoints = sharedPreferences.getAll();
+                                    List<Long> timestampList = new ArrayList<Long>();
+
+                                    // For each saved location
+                                    for (Map.Entry<String, ?> pointToSearch : sharedPoints.entrySet()) {
+                                        // If timestamp not collected yet
+                                        if (!timestampList.contains(Long.valueOf(pointToSearch.getKey().split(" ")[2]))) {
+                                            // Construct array object
+                                            JSONObject singlePoint = new JSONObject();
+
+                                            // Determine if lat/lon
+                                            if (pointToSearch.getKey().contains("Latitude")) {
+                                                for (Map.Entry<String, ?> pointToCheck : sharedPoints.entrySet()) {
+                                                    // On lon match
+                                                    if (pointToCheck.getKey().contains("Altitude") && Long.valueOf(pointToCheck.getKey().split(" ")[2]) == Long.valueOf(pointToSearch.getKey().split(" ")[2])) {
+                                                        // Add lat/lon to json location object
+                                                        Log.d(getClass().toString(), "Exporting point: " + Long.valueOf((String) pointToSearch.getValue()) * 1000000 + "/" + Long.valueOf((String) pointToCheck.getValue()) * 1000000);
+
+                                                        singlePoint.put("lat", Double.longBitsToDouble(Long.valueOf((String) pointToSearch.getValue())) * 1000000);
+                                                        singlePoint.put("lon", Double.longBitsToDouble(Long.valueOf((String) pointToCheck.getValue())) * 1000000);
+
+                                                        // Break search
+                                                        break;
+                                                    }
+                                                }
+                                            } else {
+                                                for (Map.Entry<String, ?> pointToCheck : sharedPoints.entrySet()) {
+                                                    // On lat match
+                                                    if (pointToCheck.getKey().contains("Latitude") && Long.valueOf(pointToCheck.getKey().split(" ")[2]) == Long.valueOf(pointToSearch.getKey().split(" ")[2])) {
+                                                        // Add lat/lon to json location object
+                                                        singlePoint.put("lat", Double.longBitsToDouble(Long.valueOf((String) pointToCheck.getValue())) * 1000000);
+                                                        singlePoint.put("lon", Double.longBitsToDouble(Long.valueOf((String) pointToSearch.getValue())) * 1000000);
+
+                                                        // Break search
+                                                        break;
+                                                    }
+                                                }
+                                            }
+
+                                            // Add locations to array
+                                            jsonPoints.put(singlePoint);
+
+                                            // Add timestamp to list of handled ones
+                                            timestampList.add(Long.valueOf(pointToSearch.getKey().split(" ")[2]));
+                                        }
+                                    }
+
+                                    // Add array to message
+                                    jsonMessage.put("points", jsonPoints);
+                                } catch (JSONException e) {
+                                    Log.e(getClass().toString(), e.getLocalizedMessage());
+                                }
+
+                                // Convert to string
+                                String message = jsonMessage.toString();
+
+                                // Add to intent
+                                intent.putExtra("ch.appquest.logmessage", message);
+
+                                // Export data
+                                Log.i(getClass().toString(), "Exporting data to logbook: " + message);
+                                startActivity(intent);
+
+                                // Inform user
+                                Toast.makeText(v.getContext(), "Exported data to logbook", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i(getClass().toString(), "Canceled exporting marks to logbook..");
+                                return;
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 
     @Override
@@ -166,24 +335,9 @@ public class MainActivity extends Activity implements LocationListener {
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
         // Update location listener
-        Log.i(getClass().toString(), "GPS provider enabled.");
-        if (locationManager == null) {
-            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            provider = locationManager.getBestProvider(criteria, true);
-        }
-
-        try {
-            locationManager.requestLocationUpdates(provider, 10, 10, this);
-        } catch (SecurityException e) {
-            Log.e(getClass().toString(), e.getLocalizedMessage());
-        }
+        Log.d(getClass().toString(), "Provider status changed. Updating provider..");
+        initalizeLocationTrackerAndCheckPermission();
     }
 
     @Override
@@ -209,5 +363,12 @@ public class MainActivity extends Activity implements LocationListener {
                     }
                 })
                 .show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // Update location listener
+        Log.i(getClass().toString(), "GPS provider enabled.");
+        initalizeLocationTrackerAndCheckPermission();
     }
 }
